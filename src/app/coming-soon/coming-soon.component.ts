eb64efc0c94fc9ede1c012971ecd78aaacc35c11
Philip Panyukov <ppanyukov@googlemail.com>
2019-05-30 16:47:43 +0100

    add noddy halo CSR emblems

--- src/app/coming-soon/coming-soon.component.ts
+++ src/app/coming-soon/coming-soon.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { haloRanks, haloRanksOrdered } from '../haloCsr';
 
 @Component({
   selector: 'app-coming-soon',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./coming-soon.component.scss']
 })
 export class ComingSoonComponent implements OnInit {
+  haloRanks = haloRanks;
+  haloRanksOrdered = haloRanksOrdered;
 
   constructor() { }
 
---
+++ src/app/haloCsr.ts
@@ -0,0 +1,87 @@
+export const haloRanks = {
+  // tslint:disable: max-line-length
+  unranked: {
+    title: 'Unranked',
+    rank: 0,
+    img: {
+      src: 'https://content.halocdn.com/media/Default/games/halo-5-guardians/csr/unranked_00-61fca949c33f433ba7e7507d97ff130f.png',
+      width: '256px',
+      height: '256px',
+    }
+  },
+  bronze: {
+    title: 'Bronze',
+    rank: 1,
+    img: {
+      src: 'https://content.halocdn.com/media/Default/games/halo-5-guardians/csr/csr_bronze_array01-c68c7e495d124eba8343c0874d74cd36.png',
+      width: '256px',
+      height: '256px',
+    },
+  },
+  silver: {
+    title: 'Silver',
+    rank: 2,
+    img: {
+      src: 'https://content.halocdn.com/media/Default/games/halo-5-guardians/csr/csr_silver_array01-b4739333586340bda2eed6099e630f92.png',
+      width: '256px',
+      height: '256px',
+    },
+  },
+  gold: {
+    title: 'Gold',
+    rank: 3,
+    img: {
+      src: 'https://content.halocdn.com/media/Default/games/halo-5-guardians/csr/csr_gold_array01-5053cd291bb74d6a852f0cb90a62195e.png',
+      width: '256px',
+      height: '256px',
+    },
+  },
+  plat: {
+    rank: 4,
+    title: 'Platinum',
+    img: {
+      src: 'https://content.halocdn.com/media/Default/games/halo-5-guardians/csr/csr_platinum_array01-c8df3dc366ea49209762f9b08189ffa6.png',
+      width: '256px',
+      height: '256px',
+    },
+  },
+  diamond: {
+    title: 'Diamond',
+    rank: 5,
+    img: {
+      src: 'https://content.halocdn.com/media/Default/games/halo-5-guardians/csr/csr_diamond_array01-9721d95b267942dcb1edcce6dfc25631.png',
+      width: '256px',
+      height: '256px',
+    },
+  },
+  onyx: {
+    title: 'Onyx',
+    rank: 6,
+    img: {
+      src: 'https://content.halocdn.com/media/Default/games/halo-5-guardians/csr/csr_top_array00-783f32318c8c49eda0365c5daa50f5b6.png',
+      width: '256px',
+      height: '256px',
+    },
+  },
+  champ: {
+    title: 'Champion',
+    rank: 7,
+    img: {
+      src: 'https://content.halocdn.com/media/Default/games/halo-5-guardians/csr/csr_top_array01-3755e5127c9c46368e648d58da44390e.png',
+      width: '256px',
+      height: '256px',
+    },
+  },
+};
+
+export const haloRanksOrdered = [
+  haloRanks.unranked,
+  haloRanks.bronze,
+  haloRanks.silver,
+  haloRanks.gold,
+  haloRanks.plat,
+  haloRanks.diamond,
+  haloRanks.onyx,
+  haloRanks.champ,
+];
+
