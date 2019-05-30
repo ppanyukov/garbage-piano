b5e277c4cbbf7c27803ec821542c3fcd100d2822
Philip Panyukov <ppanyukov@googlemail.com>
2019-05-30 14:12:18 +0100

    Add Angular Material
    - See https://material.angular.io/
    - Used ng add @angular/material with options:
      - prebuilt theme: Indigo/Pink: https://material.angular.io?theme=indigo-pink
      - HammerJS for gesture recognition
      - Set up browser animations for Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComingSoonComponent } from './coming-soon/coming-soon.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ComingSoonComponent } from './coming-soon/coming-soon.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
